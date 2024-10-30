08262d50f72df7cf405fbc1ec6fe245149ece083
106010343 <vovatarasov1672@gmail.com>
2024-10-30 12:55:01 +0300

    Main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("tarasov_branch");
+
+        int num1 = new Scanner(System.in).nextInt();
+
+        for (int i = 0; i <= num1; i++){
+            for (int j = num1; j > 0; j--){
+                if(i * j == num1){
+                    System.out.println(i + "*" + j);
+                }
+            }
+        }
     }
 }
